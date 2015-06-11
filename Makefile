98f89774577ce7e04f177a0ac42ed6058aec57ca
Pablo Lins <pablo.lins@ideais.com.br>
2015-06-11 15:53:01 -0300

    bug on makefile fixed

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 test:
 	make build && ./runtests
 build:
-	g++ main.cpp -Igmock-1.7.0/include -Igmock-1.7.0/gtest/include -Lgmock-1.7.0/build -lgmock -lpthread -o runtests
+	g++ main.cpp SoundexTest.cpp -Igmock-1.7.0/include -Igmock-1.7.0/gtest/include -Lgmock-1.7.0/build -lgmock -lpthread -o runtests
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,10 +1,22 @@
 #include "gmock/gmock.h"
+#include <string>
 
 class Soundex {
 
+public:
+  std::string encode(const std::string& word) const
+  {
+    return "";
+  }
 };
 
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
   Soundex soundex;
+
+  std::string encoded = soundex.encode("A");
+
+  printf("%s\n", encoded.c_str());
+
+  ASSERT_THAT(encoded, testing::Eq("A"));
 }
